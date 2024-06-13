98cef17a436913dea85b0cd81448c0d619165b57
Rishabh Singh <rishabh0739@gmail.com>
2024-06-13 14:04:02 +0530

    fix: changed DB URI

--- config/psqldb.js
+++ config/psqldb.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool
 require('dotenv').config();
 
 const pool = new Pool({
-    connectionString: process.env.PSQL_LOCAL,
+    connectionString: process.env.PSQL_URI,
 })
 
 module.exports = pool   
