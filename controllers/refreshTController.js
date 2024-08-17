f9491e9ffca6b07e6635383483cb5c3ace1c69f7
Rishabh Singh <rishabh0739@gmail.com>
2024-08-18 00:04:08 +0530

    Fixes: JWT refreshing token now available.

--- controllers/refreshTController.js
+++ controllers/refreshTController.js
@@ -1,38 +1,39 @@
-const User = require('../model/User');
 const jwt = require('jsonwebtoken');
+const pool = require('../config/psqldb');
 
 const handleRefreshToken = async (req, res) => {
     const cookies = req.cookies;
-
+    
     if (!cookies?.jwt) return res.sendStatus(401);
     
     const refreshToken = cookies.jwt;
-
+    
     const query1 ={
         text:"SELECT * FROM users WHERE refreshToken = $1",
         values: [refreshToken]
     };
-
+    
     const response = await pool.query(query1);
     const foundUser = response.rows[0];
     
     if (!foundUser) return res.sendStatus(403); //Forbidden
-
+    
     // evaluate jwt 
     jwt.verify(
         refreshToken,
         process.env.REFRESH_TOKEN_SECRET,
         (err, decoded) => {
-            if (err || foundUser.username !== decoded.username) return res.sendStatus(403);
+            if (err || foundUser.email !== decoded.email) return res.sendStatus(403);
             const accessToken = jwt.sign(
                 {
                     "UserInfo": {
-                        "username": decoded.username,
+                        "username": foundUser.username,
+                        "email": foundUser.email,
                         "_id": foundUser._id
                     }
                 },
                 process.env.ACCESS_TOKEN_SECRET,
-                { expiresIn: '10s' }
+                { expiresIn: '15m' }
             );
             res.json({ accessToken })
         }
